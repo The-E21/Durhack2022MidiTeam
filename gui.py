17a25b26c11ae1ed43a842e6b565068074e4562e
Nic Salmon <qvns53@durham.ac.uk>
2022-11-19 13:11:46 +0000

    Added an ugly quit button

--- gui.py
+++ gui.py
@@ -1,7 +1,17 @@
 import pygame
+from pygame import Rect
 import sys
 
 pygame.init()
+
+BLACK: tuple[int, int, int] = (0, 0, 0)
+WHITE: tuple[int, int, int] = (255, 255, 255)
+RED: tuple[int, int, int] = (255, 0, 0)
+LIGHT_RED: tuple[int, int, int] = (255, 204, 203)
+DARK_RED: tuple[int, int, int] = (220,20,50)
+BLUE: tuple[int, int, int] = (0, 0, 255)
+GREY: tuple[int, int, int] = (194, 196, 194)
+DARK_GREY: tuple[int, int, int] = (90, 91, 90)
 
 monitorInfoObject = pygame.display.Info()
 windowWidth: int = monitorInfoObject.current_w
@@ -17,18 +27,35 @@ windowSurface = pygame.display.set_mode((windowWidth, windowHeight))
 def mainMenu():
     timer: int = 0
     while True:
-        windowSurface.fill((255,255,255))
+        windowSurface.fill(WHITE)
+
+        quit_button: Rect = pygame.Rect(15, windowHeight - (windowHeight/9+15), windowWidth/6, windowHeight/9)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     terminate()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if quit_button.collidepoint(pygame.mouse.get_pos()):
+                    terminate()
+
+        pygame.draw.rect(windowSurface, LIGHT_RED, quit_button)
+        pygame.draw.rect(windowSurface, DARK_RED, quit_button, 10)
+        drawText("Quit", windowSurface, windowWidth/20, 8*windowHeight/9 + 5,
+                 pygame.font.SysFont('calibri', round(100 * scale)), BLACK)
 
         pygame.display.update()
 
 def terminate():
     pygame.quit()
     sys.exit()
+
+def drawText(text, surface, x, y, font, color=(255, 0, 0)):
+    textObject = font.render(text, True, color)
+    textRect = textObject.get_rect()
+    textRect.topleft = (x, y)
+    surface.blit(textObject, textRect)
 
 mainMenu()
