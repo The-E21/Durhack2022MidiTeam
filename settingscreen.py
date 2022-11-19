9d0753a6fa4062a22d4d155a3c87bb17ea3cf8ed
Nic Salmon <qvns53@durham.ac.uk>
2022-11-19 14:49:57 +0000

    Made the settings window appear

--- settingscreen.py
+++ settingscreen.py
@@ -1,2 +1,38 @@
+import pygame
+import sys
+
 def showSettings():
-    pass
+    pygame.init()
+
+    monitorInfoObject = pygame.display.Info()
+    windowWidth: int = monitorInfoObject.current_w
+    windowHeight: int = monitorInfoObject.current_h
+    scale: float = (windowHeight * windowWidth) / (1920 * 1080)
+
+    mainClock = pygame.time.Clock()
+    windowSurface = pygame.display.set_mode((windowWidth, windowHeight))
+
+    # pygame.display.set_caption("")
+    # pygame.display.set_icon(pygame.image.load('resources/logo.png'))
+
+    timer: int = 0
+    while True:
+        windowSurface.fill((255,255,255))
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    terminate()
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+def drawText(text, surface, x, y, font, color=(255, 0, 0)):
+    textObject = font.render(text, True, color)
+    textRect = textObject.get_rect()
+    textRect.topleft = (x, y)
+    surface.blit(textObject, textRect)
